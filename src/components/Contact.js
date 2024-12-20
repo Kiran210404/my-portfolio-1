d07b5ce890731ce957967d2becb7ea80b9b04040
kiran mane <137685596+Kiran210404@users.noreply.github.com>
2024-12-20 16:46:59 +0530

    commit2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <section className="contact-section">
+    <section  id="contact" className="contact-section">
       <h2>Contact</h2>
       <form action="https://formspree.io/f/xrbgnrgv" method="POST">
         <label htmlFor="name">Name:</label>
